efbf55521f79a5b916e3baa9ddefc6b666997b38
JanLynx <65616021+JanLynx@users.noreply.github.com>
2021-11-16 13:17:24 +0100

    deleted files

--- Labor04_Startfiles/buildJar.bat
+++
@@ -1,2 +0,0 @@
-jar cfve ServerRMI.jar Program Program.class RMIServer.class RMIInterface.class
-jar cfve ClientRMI.jar RMIClient RMIClient.class RMIInterface.class
--- Labor04_Startfiles/startServer.bat
+++
@@ -1,2 +0,0 @@
-start rmiregistry 5000
-java -Djava.security.policy=file:security.policy  -jar ServerRMI.jar
