ead995331c7bbf9b76f319770824710533bfb32f
JanLynx <65616021+JanLynx@users.noreply.github.com>
2021-11-16 13:16:51 +0100

    added a shortcut for building the system

---
+++ Labor04_Startfiles/compileNowAndStartServ.bat
@@ -0,0 +1,8 @@
+javac C:\Users\jansc\git\RechnerkommunikationUndMiddleware\Labor04_RMIClient\src\*.java
+javac C:\Users\jansc\git\RechnerkommunikationUndMiddleware\Labor04_RMIServer\src\*.java
+move C:\Users\jansc\git\RechnerkommunikationUndMiddleware\Labor04_RMIClient\src\*.class C:\Users\jansc\git\RechnerkommunikationUndMiddleware\Labor04_Startfiles
+move C:\Users\jansc\git\RechnerkommunikationUndMiddleware\Labor04_RMIServer\src\*.class C:\Users\jansc\git\RechnerkommunikationUndMiddleware\Labor04_Startfiles
+jar cfve ServerRMI.jar Program Program.class RMIServer.class RMIInterface.class
+jar cfve ClientRMI.jar RMIClient RMIClient.class RMIInterface.class
+start rmiregistry 5000
+java -Djava.security.policy=file:security.policy  -jar ServerRMI.jar
