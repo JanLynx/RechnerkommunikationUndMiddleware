1e59c3b95857ee352023cdc1ee2a04d1516487cb
JanLynx <65616021+JanLynx@users.noreply.github.com>
2021-11-17 14:31:40 +0100

    fixed errors, cleaned code

--- Labor04_RMIServer/src/DOMBuilder.java
+++ Labor04_RMIServer/src/DOMBuilder.java
@@ -4,21 +4,16 @@ import org.w3c.dom.*;
 public class DOMBuilder {
 
 	private Document doc;
-	
 	private String charCounterString;
 	private String wordCounterString;
 	private String eCounterString;
-
-	public DOMBuilder() {
-
-	}
 
 	public Document buildDocument(String charCounterString, String wordCounterString, String eCounterString) {
 		try {
 			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
 			DocumentBuilder builder = dbf.newDocumentBuilder();
 			doc = builder.newDocument();
-			Element textanalyse = doc.createElement("catalog");
+			Element textanalyse = doc.createElement("textanalyse");
 			doc.appendChild(textanalyse);
 			Element charCounter = doc.createElement("charCounter");
 			charCounter.appendChild(doc.createTextNode(charCounterString));
@@ -36,51 +31,34 @@ public class DOMBuilder {
 		return doc;
 	}
 
-//	public void saveDocument(String filename) throws TransformerException, IOException {
-//		TransformerFactory transformerFactory = TransformerFactory.newInstance();
-//		Transformer transformer = transformerFactory.newTransformer();
-//		DOMSource source = new DOMSource(doc);
-//		StreamResult result = new StreamResult(new File(filename));
-//		transformer.transform(source, result);
-//		String xmlDocument = Files.readString(Path.of(filename));
-//		System.out.println(xmlDocument);
-//	}
-	
 	public void parseDocument(Document doc) {
 		try {
-//	         File inputFile = new File("input.txt");
-//	         DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-//	         DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-//	         Document doc = dBuilder.parse(inputFile);
-	         doc.getDocumentElement().normalize();
-	         NodeList nList = doc.getElementsByTagName("textanalyse");
-	         
-	         for (int temp = 0; temp < nList.getLength(); temp++) {
-	            Node nNode = nList.item(temp);
-	            System.out.println("\nCurrent Element :" + nNode.getNodeName());
-	            
-	            if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-	               Element eElement = (Element) nNode;
-//	               filename = eElement.getAttribute("Datei"));
-	               charCounterString = eElement.getElementsByTagName("charCounter").item(0).getTextContent();
-	               wordCounterString = eElement.getElementsByTagName("wordCounter").item(0).getTextContent();
-	               eCounterString = eElement.getElementsByTagName("eCounter").item(0).getTextContent();
-	            }
-	         }
-	      } catch (Exception e) {
-	         e.printStackTrace();
-	      }
+			doc.getDocumentElement().normalize();
+			NodeList nList = doc.getElementsByTagName("textanalyse");
+
+			for (int temp = 0; temp < nList.getLength(); temp++) {
+				Node nNode = nList.item(temp);
+				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+					Element eElement = (Element) nNode;
+					charCounterString = eElement.getElementsByTagName("charCounter").item(0).getTextContent();
+					wordCounterString = eElement.getElementsByTagName("wordCounter").item(0).getTextContent();
+					eCounterString = eElement.getElementsByTagName("eCounter").item(0).getTextContent();
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
-	
-	public String getCharCounter() {
-		return charCounterString;
+
+	public int getCharCounter() {
+		return Integer.parseInt(charCounterString);
 	}
-	
-	public String getWordCounter() {
-		return wordCounterString;
+
+	public int getWordCounter() {
+		return Integer.parseInt(wordCounterString);
 	}
-	
-	public String getECounter() {
-		return eCounterString;
+
+	public int getECounter() {
+		return Integer.parseInt(eCounterString);
 	}
 }
--- Labor04_Startfiles/compileNowAndStartServ.bat
+++ Labor04_Startfiles/compileNowAndStartServ.bat
@@ -2,7 +2,7 @@ javac C:\Users\jansc\git\RechnerkommunikationUndMiddleware\Labor04_RMIClient\src
 javac C:\Users\jansc\git\RechnerkommunikationUndMiddleware\Labor04_RMIServer\src\*.java
 move C:\Users\jansc\git\RechnerkommunikationUndMiddleware\Labor04_RMIClient\src\*.class C:\Users\jansc\git\RechnerkommunikationUndMiddleware\Labor04_Startfiles
 move C:\Users\jansc\git\RechnerkommunikationUndMiddleware\Labor04_RMIServer\src\*.class C:\Users\jansc\git\RechnerkommunikationUndMiddleware\Labor04_Startfiles
-jar cfve ServerRMI.jar Program Program.class RMIServer.class RMIInterface.class
-jar cfve ClientRMI.jar RMIClient RMIClient.class RMIInterface.class
+jar cfve ServerRMI.jar Program Program.class RMIServer.class RMIInterface.class DOMBuilder.class
+jar cfve ClientRMI.jar RMIClient RMIClient.class RMIInterface.class DOMBuilder.class
 start rmiregistry 5000
 java -Djava.security.policy=file:security.policy  -jar ServerRMI.jar
